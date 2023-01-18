3ffce95edadb28eea2e47e8fb0228045ac824b1e
ALX <3496916+alexplong@users.noreply.github.com>
2023-01-18 16:34:40 -0500

    - Initial template updated. - Working on selected onClick handler

---
+++ src/component/Main.js
@@ -0,0 +1,67 @@
+/** @format */
+
+import React from "react";
+
+function Main() {
+  const [tenDice, setTenDice] = React.useState([
+    { id: 1, roll: 1, selected: false },
+    { id: 2, roll: 1, selected: false },
+    { id: 3, roll: 1, selected: true },
+    { id: 4, roll: 1, selected: false },
+    { id: 5, roll: 1, selected: false },
+    { id: 6, roll: 1, selected: true },
+    { id: 7, roll: 1, selected: false },
+    { id: 8, roll: 1, selected: false },
+    { id: 9, roll: 1, selected: false },
+    { id: 10, roll: 1, selected: false },
+  ]);
+
+  const diceElements = tenDice.map((die) => {
+    return (
+      <div
+        key={die.id}
+        id={die.id}
+        className={die.selected ? "dice selected" : "dice"}
+        onClick={(event) => handleClick(event)}
+      >
+        {die.roll}
+      </div>
+    );
+  });
+
+  function handleClick(event) {
+    const { id } = event.target;
+    // console.log(id);
+    console.log(tenDice[0]);
+    setTenDice((prevDices) => {
+      return prevDices.map((die) => {
+        return die.id === id ? { ...die, selected: !die.selected } : die;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <div className="outer--container">
+        <div className="inner--container">
+          <br />
+          <br />
+          <div className="text_wrapper">
+            <h1 className="title">Tenzies</h1>
+          </div>
+          <div className="subtitle_wrapper">
+            <h3 className="subtitle">
+              Roll until all dice are the same. Click each die to freeze it at
+              its current value between rolls.
+            </h3>
+          </div>
+          <br />
+          <br />
+          <div className="dice_wrapper">{diceElements}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
